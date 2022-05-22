387ea5d9e41b135ae6cc80df83b476f0f1c3cad0
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-05-21 21:33:57 -0300

    frontend initial structure

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -5,8 +5,8 @@ export default function Map() {
     return (
         <Template>
             <h1>MAP</h1>
-            <Link href="/register">
-                <button>Register New Location</button>
+            <Link href="/">
+                <button>Go to map</button>
             </Link>
         </Template>
     )
