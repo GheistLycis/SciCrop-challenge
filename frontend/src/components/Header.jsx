b40e1a20ad5c0bbc35ccd11ae34078e5fbf1062c
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-05-23 11:24:06 -0300

    minor styling fixes

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,8 +6,10 @@ import img from "../public/logo_scicrop.png"
 export default function Header({ btnLink, btnText }) {
     return (
         <div className={styles.header}>
-            <img src={img} alt="sci_crop leaf" className={styles.leaf}></img>
-            <h1 className={styles.scicrop}>SciCrop</h1>
+            <div className={styles.logo}>
+                <img src={img} alt="sci_crop leaf" className={styles.leaf}></img>
+                <h1 className={styles.scicrop}>SciCrop</h1>
+            </div>
             <button className={styles.btn}><Link to={btnLink}>{btnText}</Link></button>
         </div>
     )
